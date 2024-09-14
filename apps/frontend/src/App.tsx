6ecc17a0f1fdf24328513e5bc9d3905752b276c2
sinho.lee <cotesinho123@gmail.com>
2024-09-14 15:00:12 +0900

    feat: 가격대별 구매 빈도 차트 - datepicker 상태 및 에러 정의

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
+import PurchaseFrequencyChart from './components/purchase-frequency-chart'
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <>
-      <h1>Counter</h1>
-      <p>{count} </p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <PurchaseFrequencyChart />
     </>
   )
 }
---
+++ apps/frontend/src/components/purchase-frequency-chart/index.tsx
@@ -0,0 +1,12 @@
+import { DateRangeField } from './date-range-feild.tsx'
+
+const PurchaseFrequencyChart = () => {
+  return (
+    <article>
+      <h1>가격대별 구매 빈도 차트</h1>
+      <DateRangeField />
+    </article>
+  )
+}
+
+export default PurchaseFrequencyChart
