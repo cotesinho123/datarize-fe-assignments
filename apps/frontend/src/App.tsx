e54277ed3683828ea546d45a65f71e6970cfb902
sinho.lee <cotesinho123@gmail.com>
2024-09-14 13:42:03 +0900

    feat: 기본 app.tsx 설정

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,15 +1,14 @@
-import {useState} from 'react'
-
+import { useState } from 'react'
 
 function App() {
-    const [count, setCount] = useState(0)
-    return (
-        <>
-            <h1>Counter</h1>
-            <p>{count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-        </>
-    )
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <h1>Counter</h1>
+      <p>{count} </p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </>
+  )
 }
 
 export default App
